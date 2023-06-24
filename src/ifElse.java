0704badd6c938814f99b049b79ae5ef64f28e9d3
Nykuluz <carlmillanes3@gmail.com>
2023-06-25 07:29:46 +0800

    If Boolean Implementation

--- src/ifElse.java
+++ src/ifElse.java
@@ -1,2 +1,8 @@
 public class ifElse {
+    public static void main(String[] args){
+        boolean isDone = false;
+        if(!isDone){
+            System.out.println("It's not done yet");
+        }
+    }
 }
