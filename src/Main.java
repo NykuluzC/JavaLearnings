59ebca5fec733d8e1670311b2dc442bf38f087ef
Nykuluz <carlmillanes3@gmail.com>
2023-06-25 06:17:38 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("This is my Java Learnings!");
+        int x = 5;
+        int y = x + 5;
+        System.out.println(x);
+    }
+}
